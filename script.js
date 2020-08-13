7e541f47847224a8a8abdf7f8304ca49b7dcada5
Shawanda Ford <shawanda.ford@gmail.com>
2020-08-13 14:39:42 -0400

    update table with timeblocks

---
+++ script.js
@@ -0,0 +1,12 @@
+//Get div and display current time
+$("#currentDay").html(moment().format('LLLL'));
+
+// //Time Block Div
+$("container").html("<span>")
+   var after=$("<span>")
+   after.text("test")
+   $("#after").append(after)
+    container.attr("class", "after");
+
+
+
