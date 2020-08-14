8c3204df67cec40b1c7f8da99de03aef4c222cd3
Shawanda Ford <shawanda.ford@gmail.com>
2020-08-13 21:57:16 -0400

    updated the readMe

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,33 +1,13 @@
 //Get div and display current time
 $("#currentDay").html(moment().format('LLLL'));
 
-//Get TimeBlock and set attribute
-// $(".block8").html("test")
-// $(".block8").attr("class","form-control")
-// $(".block9").html("test")
-// $(".block9").attr("class","form-control")
-// $(".block10").html("test")
-// $(".block10").attr("class","form-control")
-// $(".block11").html("test")
-// $(".block11").attr("class","form-control")
-// $(".block12").html("test")
-// $(".block12").attr("class","form-control")
-// $(".block1").html("test")
-// $(".block1").attr("class","form-control")
-// $(".block2").html("test")
-// $(".block2").attr("class","form-control")
-// $(".block3").html("test")
-// $(".block3").attr("class","form-control")
-// $(".block4").html("test")
-// $(".block4").attr("class","form-control")
-// $(".block5").html("test")
-// $(".block5").attr("class","form-control")
 
-//Set timeblock color codes
+//Set timeblock and color codes
 
+//Get Current time
 var currentHour = moment().format("H")
 
-
+//add color codes based on current time
 for (var i=8; i<18; i++){
     if(currentHour > i){
         $(".block"+i).addClass("past")
@@ -36,13 +16,14 @@ for (var i=8; i<18; i++){
     } else{
         $(".block"+i).addClass("future")
     }
+    //get event and hour selected by the user
     var local=localStorage.getItem("hour"+i)
     if(local !== null){
         $(".block"+i).text(local)
 
     }
 }
-
+//save selected hour and text entered by the user
 $(".btn").click(function(){
     var selectedHour = $(this).val()
     var textToSave=$(".block"+selectedHour).val()
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Day-Planner
 
 ## Description
-
-
+This is an application that will allow the end user to schedule their work day by saving events and/or task for each hour of the day. The app uses HTML, CSS and jQuery.
 
 ## Screenshot of website  
 ![Screenshot of deployed site](./Assets/screenshot.PNG)
+
 ## Deployed site
 
 [Day-Planner](https://sford4186.github.io/Day-Planner/)
